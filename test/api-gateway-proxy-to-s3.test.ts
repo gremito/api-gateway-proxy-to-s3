272eb437530c32873695adc6c4c39367508e4ec6
gremito <grem.takuya0223@gmail.com>
2023-01-19 16:56:35 +0900

    Initial commit

---
+++ test/api-gateway-proxy-to-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiGatewayProxyToS3 from '../lib/api-gateway-proxy-to-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-gateway-proxy-to-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiGatewayProxyToS3.ApiGatewayProxyToS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
