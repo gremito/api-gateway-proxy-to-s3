4dd96b490c04117d1ca5883f65b8eee2dea6fa9f
gremito <grem.takuya0223@gmail.com>
2023-01-19 19:37:58 +0900

    first push

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# Welcome to your CDK TypeScript project
+# API Gateway Proxy to S3
+
+ref: 
+
+- https://aws.amazon.com/jp/getting-started/guides/setup-cdk/module-three/
+- https://dev.classmethod.jp/articles/sam-local-cdk-typescript/
+- https://dev.classmethod.jp/articles/api-gateway-proxy-to-s3-put-object-by-cdk/
+
+## Welcome to your CDK TypeScript project
 
 This is a blank project for CDK development with TypeScript.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
-## Useful commands
+### Useful commands
 
 * `npm run build`   compile typescript to js
 * `npm run watch`   watch for changes and compile
--- lib/api-gateway-proxy-to-s3-stack.ts
+++ lib/api-gateway-proxy-to-s3-stack.ts
@@ -1,16 +1,214 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs";
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  aws_apigateway as apigateway,
+  aws_lambda_nodejs as lambda,
+  aws_iam as iam
+} from "aws-cdk-lib";
 
-export class ApiGatewayProxyToS3Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ApiGatewayProxyToS3Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const projectName: string = this.node.tryGetContext("projectName");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiGatewayProxyToS3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda
+    new lambda.NodejsFunction(this, "MyLambda", {
+      entry: "lambda/app.ts",
+      handler: "lambdaHandler",
+    });
+
+    // S3
+    const bucket = new s3.Bucket(this, "Bucket", {
+      bucketName: `${projectName}-bucket`,
+    });
+
+    // API Gateway
+    const restApiRole = new iam.Role(this, "Role", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      path: "/",
+    });
+    bucket.grantReadWrite(restApiRole);
+
+    // REST API
+    const restApi = new apigateway.RestApi(this, "RestApi", {
+      restApiName: `${projectName}-api`,
+      deployOptions: {
+        stageName: "v1",
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: ["POST", "OPTIONS", "PUT", "DELETE"],
+        statusCode: 200,
+      },
+      binaryMediaTypes: ["image/*"],
+    });
+
+    const users = restApi.root.addResource("users");
+    const userId = users.addResource("{userId}");
+    const files = userId.addResource("files");
+    const fileName = files.addResource("{fileName}");
+
+    const integrationResponseParametersOfCors = {
+      "method.response.header.Access-Control-Allow-Headers":
+        "'Content-Type,Authorization'",
+      "method.response.header.Access-Control-Allow-Methods":
+        "'OPTIONS,POST,PUT,GET,DELETE'",
+      "method.response.header.Access-Control-Allow-Origin": "'*'",
+    };
+
+    const integrationResponses = [
+      {
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Timestamp":
+            "integration.response.header.Date",
+          "method.response.header.Content-Length":
+            "integration.response.header.Content-Length",
+          "method.response.header.Content-Type":
+            "integration.response.header.Content-Type",
+          ...integrationResponseParametersOfCors,
+        },
+      },
+      {
+        statusCode: "400",
+        selectionPattern: "4\\d{2}",
+        responseParameters: integrationResponseParametersOfCors,
+      },
+      {
+        statusCode: "500",
+        selectionPattern: "5\\d{2}",
+        responseParameters: integrationResponseParametersOfCors,
+      },
+    ];
+
+    const methodResponseParametersOfCors = {
+      "method.response.header.Access-Control-Allow-Headers": true,
+      "method.response.header.Access-Control-Allow-Methods": true,
+      "method.response.header.Access-Control-Allow-Origin": true,
+    };
+
+    const methodResponses = [
+      {
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Timestamp": true,
+          "method.response.header.Content-Length": true,
+          "method.response.header.Content-Type": true,
+          ...methodResponseParametersOfCors,
+        },
+      },
+      {
+        statusCode: "400",
+        responseParameters: methodResponseParametersOfCors,
+      },
+      {
+        statusCode: "500",
+        responseParameters: methodResponseParametersOfCors,
+      },
+    ];
+
+    files.addMethod(
+      "GET",
+      new apigateway.AwsIntegration({
+        service: "s3",
+        integrationHttpMethod: "GET",
+        path: bucket.bucketName,
+        options: {
+          credentialsRole: restApiRole,
+          passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+          integrationResponses,
+          requestTemplates: {
+            "application/json": `#set($context.requestOverride.querystring.prefix = "$input.params('userId')/")
+#set($context.requestOverride.querystring.delimiter = "/")`,
+          },
+        },
+      }),
+      { methodResponses }
+    );
+
+    fileName.addMethod(
+      "GET",
+      new apigateway.AwsIntegration({
+        service: "s3",
+        integrationHttpMethod: "GET",
+        path: `${bucket.bucketName}/{folder}/{object}`,
+        options: {
+          credentialsRole: restApiRole,
+          passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+          requestParameters: {
+            "integration.request.header.Accept": "method.request.header.Accept",
+            "integration.request.path.folder": "method.request.path.userId",
+            "integration.request.path.object": "method.request.path.fileName",
+          },
+          integrationResponses,
+        },
+      }),
+      {
+        requestParameters: {
+          "method.request.header.Accept": true,
+          "method.request.path.userId": true,
+          "method.request.path.fileName": true,
+        },
+        methodResponses,
+      }
+    );
+
+    fileName.addMethod(
+      "PUT",
+      new apigateway.AwsIntegration({
+        service: "s3",
+        integrationHttpMethod: "PUT",
+        path: `${bucket.bucketName}/{folder}/{object}`,
+        options: {
+          credentialsRole: restApiRole,
+          passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+          requestParameters: {
+            "integration.request.header.Content-Type":
+              "method.request.header.Content-Type",
+            "integration.request.path.folder": "method.request.path.userId",
+            "integration.request.path.object": "method.request.path.fileName",
+          },
+          integrationResponses,
+        },
+      }),
+      {
+        requestParameters: {
+          "method.request.header.Content-Type": true,
+          "method.request.path.userId": true,
+          "method.request.path.fileName": true,
+        },
+        methodResponses,
+      }
+    );
+
+    fileName.addMethod(
+      "DELETE",
+      new apigateway.AwsIntegration({
+        service: "s3",
+        integrationHttpMethod: "DELETE",
+        path: `${bucket.bucketName}/{folder}/{object}`,
+        options: {
+          credentialsRole: restApiRole,
+          passthroughBehavior: apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+          requestParameters: {
+            "integration.request.path.folder": "method.request.path.userId",
+            "integration.request.path.object": "method.request.path.fileName",
+          },
+          integrationResponses,
+        },
+      }),
+      {
+        requestParameters: {
+          "method.request.path.userId": true,
+          "method.request.path.fileName": true,
+        },
+        methodResponses,
+      }
+    );
   }
 }
